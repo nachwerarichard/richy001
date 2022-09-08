bf32a4eaae4c9af24262657c111b85f964282280
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2022-09-08 13:41:17 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h3> Full Name:Nach Rch</h3>
+     <p>Richard is a frond end web developer with three years experience</p>
+     <h2> Why you should learn react</h2>
+     <p>Udemy, Inc. is a for-profit massive open online course provider aimed at professional adults and students. It was founded in May 2010 by Eren Bali, Gagan Biyani, and Oktay Caglar. As of September of 2021, the platform has more than 46 millioUdemy, Inc. is a for-profit massive open online course provider aimed at professional adults and students. It was founded in May 2010 by Eren Bali, Gagan Biyani, and Oktay Caglar. As of September of 2021, the platform has more than 46 millio</p>
+    </div>
+  );
+}
+
+export default App;
